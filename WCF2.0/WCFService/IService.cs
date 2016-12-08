9f9366ed84090435f12905bdcedc5a6ccdee01bd
dodo <adodo1@126.com>
2016-12-08 11:47:23 +0800

    提交源码

---
+++ WCF2.0/WCFService/IService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.ServiceModel.Web;
+using System.IO;
+
+namespace WCFService
+{
+    // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码和配置文件中的接口名“IService”。
+    [ServiceContract]
+    public interface IService
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, UriTemplate = "Say")]
+        string Say();
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", ResponseFormat = WebMessageFormat.Json, UriTemplate = "SayHello/{value}/{a}")]
+        string SayHello(string value,string a);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "Speak")]
+        string Speak(Stream stream);
+
+        [OperationContract]
+        [WebInvoke(Method = "POST", BodyStyle = WebMessageBodyStyle.WrappedRequest, RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json, UriTemplate = "SpeakHello")]
+        string SpeakHello(string a, string uid); 
+    }
+}
---
+++ WCF2.0/WCFService/Service.svc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFService
+{
+    // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码、svc 和配置文件中的类名“Service”。
+    public class Service : IService
+    {
+        public string Say()
+        {
+            return "HelloWorld";
+        }
+
+        public string SayHello(string value, string a)
+        {
+            return "Hello：" + value + "," + a;
+        }
+
+        public string Speak(Stream stream)
+        {
+            StreamReader reader = new StreamReader(stream);
+            string value = reader.ReadToEnd();
+            return "Hello：Android " + value;
+        }
+
+        public string SpeakHello(string a, string uid)
+        {
+            return "Hello：Android +Listen:" + a + "_" + uid;
+        }
+    }
+}
